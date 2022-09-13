eb5970ee09681d43dfa09eb6fa49fdc731374afa
Firmanwijaya <firmanwijayadinata90@gmail.com>
2022-09-13 18:07:10 +0700

    update api in trending

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import { Card, Image, Container, Row, Col } from "react-bootstrap";
-import captainAmericaImage from "../assets/images/trending/captainamerica.jpg";
-import hulkImage from "../assets/images/trending/hulk.jpg";
-import lokiImage from "../assets/images/trending/loki.jpg";
-import morbiusImage from "../assets/images/trending/morbius.jpg";
-import thorImage from "../assets/images/trending/thor.jpg";
-import venomImage from "../assets/images/trending/venom.jpg";
+import axios from "axios";
 
 const Trending = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/discover/movie`, {
+        params: {
+          api_key: process.env.REACT_APP_TMDB_KEY,
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }, []);
+
   return (
     <div>
       <Container>
@@ -16,78 +25,22 @@ const Trending = () => {
         </h1>
         <br />
         <Row>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={captainAmericaImage} alt="Captain America image" className="images" />
-              <div className="bg-dark">
-                <div className="text-white p-2 m-1">
-                  <Card.Title className="text-center">Captain America</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={hulkImage} alt="Hulk image" className="images" />
-              <div className="bg-dark">
-                <div className="text-white p-2 m-1">
-                  <Card.Title className="text-center">Hulk</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={lokiImage} alt="Loki image" className="images" />
-              <div className="bg-dark">
-                <div className="text-white p-2 m-1">
-                  <Card.Title className="text-center">Loki</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={morbiusImage} alt="Morbius image" className="images" />
-              <div className="bg-dark">
-                <div className="text-white p-2 m-1">
-                  <Card.Title className="text-center">Morbius</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={thorImage} alt="Thor image" className="images" />
-              <div className="bg-dark">
-                <div className="text-white p-2 m-1">
-                  <Card.Title className="text-center">Thor</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={venomImage} alt="Venom image" className="images" />
-              <div className="bg-dark">
-                <div className="text-white p-2 m-1">
-                  <Card.Title className="text-center">Venom</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
+          {movies.map((result, index) => {
+            return (
+              <Col md={4} className="movieWrapper" key={index}>
+                <Card className="movieImage">
+                  <Image src={`${process.env.REACT_APP_IMG_URL}/${result.poster_path}`} alt="test image" className="images" />
+                  <div className="bg-dark">
+                    <div className="text-white p-2 m-1">
+                      <Card.Title className="text-center">{result.title}</Card.Title>
+                      <Card.Text className="text-left">{result.overview}</Card.Text>
+                      <Card.Text className="text-end">{result.release_date}</Card.Text>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </div>
